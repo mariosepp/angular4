3eaf19e4629963124f6dcff0ae4d5207a056876d
Mario Sepp <mariosepp92@gmail.com>
2017-07-11 22:01:52 +0300

    ASG-03 Directives

---
+++ src/app/logs/logs.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logs',
+  templateUrl: './logs.component.html',
+  styleUrls: ['./logs.component.css']
+})
+export class LogsComponent {
+  showSecret = false;
+  logs = [];
+
+  onDisplayDetails() {
+    this.showSecret = this.showSecret ? false : true;
+    this.logs.push(new Date());
+  }
+
+  getColor(log) {
+    return this.isGreaterThanFourth(log) ? 'blue' : 'white';
+  }
+
+  isGreaterThanFourth(log) {
+    return this.logs.indexOf(log) > 3;
+  }
+
+}
